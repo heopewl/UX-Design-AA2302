19882710ed382de5cc55fa88fad558672564670a
AimanReza06 <137847226+AimanReza06@users.noreply.github.com>
2023-08-03 14:33:57 +0800

    sdf

--- UX Offical/JS/java_script_for payment.js
+++ UX Offical/JS/java_script_for payment.js
@@ -3,9 +3,3 @@ var gangops = document.getElementById("check_in").value;
 
 document.getElementById("testing_testing").innerText='gang';
 
-
-document.getElementById("information_days_counter_date").innerText='gang';
-
-alert("yo")
-
-
