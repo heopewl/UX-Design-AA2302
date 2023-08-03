74c1d34bf0096267101522b4f458719f0f5b1940
AimanReza06 <137847226+AimanReza06@users.noreply.github.com>
2023-08-03 14:52:22 +0800

    s

--- UX Offical/JS/java_script_for payment.js
+++ UX Offical/JS/java_script_for payment.js
@@ -1,5 +1,13 @@
 
-var gangops = document.getElementById("check_in").value;
+alert("connected to the JS");
 
 document.getElementById("testing_testing").innerText='gang';
+
+document.getElementById("testing_123").innerText="please";
+
+
+//var gangops = document.getElementById("check_in").value;
+
+
+
 
